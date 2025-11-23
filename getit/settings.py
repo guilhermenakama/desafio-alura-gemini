56be814af240ab31605d2e2ef2bc502c764c358e
Guilherme Nakama <guilhermemyn@al.insper.edu.br>
2025-11-22 21:22:01 -0300

    framework Django

--- getit/settings.py
+++ getit/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = 'django-insecure-fi(=@etxwct0&!j%_e46ym_9+im-!%16^jc$%d1u%o)^e(3ao)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = ['desafio-alura-gemini.onrender.com', 'localhost', '127.0.0.1', '0.0.0.0']
 
 # Application definition
 
